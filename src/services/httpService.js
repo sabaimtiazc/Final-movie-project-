68281f4f8429be5b39dbe8489457fe0024ad8619
unknown <sabaimtiaza@gmail.com>
2020-10-04 13:21:11 -0700

    Fixing Bi-directional Dependencies

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 import logger from "./logService";
-import auth from './authService';
 import { toast } from "react-toastify";
 
 
 
-axios.defaults.headers.common['x-auth-token'] = auth.getJwt();
+
 axios.interceptors.response.use(null, error => {
   const expectedError =
     error.response &&
@@ -21,9 +20,14 @@ axios.interceptors.response.use(null, error => {
   return Promise.reject(error);
 });
 
+function setJwt(jwt) {
+  axios.defaults.headers.common["x-auth-token"] = jwt;
+}
+
 export default {
   get: axios.get,
   post: axios.post,
   put: axios.put,
-  delete: axios.delete
+  delete: axios.delete,
+  setJwt
 };
